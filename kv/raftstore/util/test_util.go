1c1fda251822a2b88681d8311dad10cf13f1db6e
lijiagui_root <lijiagui159@outlook.com>
2021-05-15 22:48:45 +0800

    raft工具及测试类

---
+++ kv/raftstore/util/test_util.go
@@ -0,0 +1,37 @@
+package util
+
+import (
+	"github.com/Connor1996/badger"
+	"io/ioutil"
+	"miniLinkDB/kv/util/engine_util"
+)
+
+func NewTestEngines() *engine_util.Engines {
+	engines := new(engine_util.Engines)
+	var err error
+	engines.KvPath, err = ioutil.TempDir("","linkkv_kv")
+	if err != nil {
+		panic("create kv dir failed")
+	}
+	kvOpts := badger.DefaultOptions
+	kvOpts.Dir = engines.KvPath
+	kvOpts.ValueDir = engines.KvPath
+	kvOpts.ValueThreshold = 256
+	engines.Kv, err = badger.Open(kvOpts)
+	if err != nil {
+		panic("open kv db failed")
+	}
+	engines.RaftPath, err = ioutil.TempDir("", "linkkv_raft")
+	if err != nil {
+		panic("create raft dir failed")
+	}
+	raftOpts := badger.DefaultOptions
+	raftOpts.Dir = engines.RaftPath
+	raftOpts.ValueDir = engines.RaftPath
+	raftOpts.ValueThreshold = 256
+	engines.Raft, err = badger.Open(raftOpts)
+	if err != nil {
+		panic("open raft db failed")
+	}
+	return engines
+}
